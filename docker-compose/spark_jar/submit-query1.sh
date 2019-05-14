25c8bb4b9a21f061a4d6b7be85e056e9e7c1ffea
Antonio Schiazza <antonio.schiazza@gmail.com>
2019-05-14 13:00:09 +0200

    update gitignore

---
+++ docker-compose/spark_jar/submit-query1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$SPARK_HOME/bin/spark-submit --class "queries.FirstQuerySolver" --master "local" /sabd/jar/SABD-project1-1.0-SNAPSHOT.jar
---
+++ docker-compose/spark_jar/submit-query2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$SPARK_HOME/bin/spark-submit --class "queries.SecondQuerySolver" --master "local" /sabd/jar/SABD-project1-1.0-SNAPSHOT.jar
