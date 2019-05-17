f06e1a59548a467bef6036b0a2fa3761d385642c
Antonio Schiazza <antonio.schiazza@gmail.com>
2019-05-17 02:42:14 +0200

    autostart of /sabd/start-services.sh

--- docker-images/hdfs-yarn-spark/hdfs-config/bootstrap.sh
+++ docker-images/hdfs-yarn-spark/hdfs-config/bootstrap.sh
@@ -4,7 +4,12 @@ sudo $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 
 rm /tmp/*.pid
 service ssh start
-# $HADOOP_PREFIX/sbin/start-dfs.sh
+#$HADOOP_PREFIX/sbin/start-dfs.sh
+
+if [ $ISMASTER -eq 1 ]
+then
+    /sabd/start-services.sh
+fi
 
 # Launch bash console  
 /bin/bash
