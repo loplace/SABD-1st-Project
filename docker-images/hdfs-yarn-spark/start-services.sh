152fd06f4f92f2678552df902178729ecf495152
Antonio Schiazza <antonio.schiazza@gmail.com>
2019-05-23 03:14:15 +0200

    added history spark dashboard

--- docker-images/hdfs-yarn-spark/start-services.sh
+++ docker-images/hdfs-yarn-spark/start-services.sh
@@ -4,4 +4,9 @@ $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 $SPARK_HOME/sbin/start-all.sh
 
+sleep 30
+
 hdfs dfs -chmod -R 777 /
+hdfs dfs -mkdir /spark-events
+hdfs dfs -mkdir /spark-logs
+$SPARK_HOME/sbin/start-history-server.sh
--- docker-images/hdfs-yarn-spark/stop-services.sh
+++ docker-images/hdfs-yarn-spark/stop-services.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+$SPARK_HOME/sbin/stop-history-server.sh
 $SPARK_HOME/sbin/stop-all.sh
 $HADOOP_HOME/sbin/stop-yarn.sh
 $HADOOP_HOME/sbin/stop-dfs.sh
